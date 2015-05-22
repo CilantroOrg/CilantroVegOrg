67d0eae85f098ebe3f1444a33afc5aaaafddfb37
David Fox-Powell <dtothefp@gmail.com>
2015-05-22 16:43:57 -0400

    add oform demo

--- website-guts/assets/js/components/modal-signin-sso.js
+++ website-guts/assets/js/components/modal-signin-sso.js
@@ -1,6 +1,14 @@
-window.optly.mrkt.form.signin({formId: 'signin-sso-form'});
-
 var signinSSOForm = new Oform({
   selector: '#signin-sso-form',
   middleware: w.optly.mrkt.Oform.defaultMiddleware
+}).on('error', function(){
+  window.alert('error');
+}).on('load', function(e){
+  //http://localhost:9000/sp_initiated_signin you should mock this enpoint in
+  //grunt/connect because it is returning 404
+  console.log(e);
+  window.alert('load');
+}).on('done', function(){
+  //this always fires which sucks
+  window.alert('done');
 });
