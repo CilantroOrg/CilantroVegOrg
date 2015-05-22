a163e8a472c27654f5f6cf766468c873cb59a282
Daniel O'Connor <daniel@optimizely.com>
2015-05-22 13:22:50 -0700

    Attempt to use oForm.

--- website-guts/assets/js/components/modal-signin-sso.js
+++ website-guts/assets/js/components/modal-signin-sso.js
@@ -1,1 +1,6 @@
 window.optly.mrkt.form.signin({formId: 'signin-sso-form'});
+
+var signinSSOForm = new Oform({
+  selector: '#signin-sso-form',
+  middleware: w.optly.mrkt.Oform.defaultMiddleware
+});
