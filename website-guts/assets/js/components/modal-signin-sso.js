10063b38970221441987a972a25dd67770fc31d0
Daniel O'Connor <daniel@optimizely.com>
2015-05-22 15:08:41 -0700

    Fix linter errors.

--- website-guts/assets/js/components/modal-signin-sso.js
+++ website-guts/assets/js/components/modal-signin-sso.js
@@ -1,12 +1,12 @@
-var signinSSOForm = new Oform({
+new Oform({
   selector: '#signin-sso-form',
   middleware: w.optly.mrkt.Oform.defaultMiddleware
 }).on('error', function(){
   window.alert('error');
-}).on('load', function(e){
+}).on('load', function(){
   //http://localhost:9000/sp_initiated_signin you should mock this enpoint in
   //grunt/connect because it is returning 404
-  console.log(e);
+  // console.log(e);
   window.alert('load');
 }).on('done', function(){
   //this always fires which sucks
