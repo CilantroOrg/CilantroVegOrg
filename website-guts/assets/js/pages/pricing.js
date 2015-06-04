44439239d4f4ec82d5a92bc2391e8d55cf9df08e
Bradley Heinz <bheinz@us-bheinz1.local>
2015-06-04 12:59:49 -0700

    Disable Get Started Now button after user clicks once

--- website-guts/assets/js/pages/pricing.js
+++ website-guts/assets/js/pages/pricing.js
@@ -90,6 +90,10 @@ var updatePlanInfo = function(){
       } else {
         //user is signed in, but no plan
         //sign the user up for the starter plan
+
+        //prevents user from creating multiple accounts
+        $('#feature-list-get-started-now').off('click');
+        console.log('Kicking off changePlan util function.');
         document.body.classList.add('processing-free-light');
         w.optly.mrkt.changePlanHelper.changePlan({
           plan: 'free_light',
