303938b81f0822b5b234899c76e33d2d0ad232ad
Stewart Smith <stewart@optimizely.com>
2015-06-10 17:27:27 -0700

    page working with js complete

--- website-guts/assets/js/pages/solutions.js
+++ website-guts/assets/js/pages/solutions.js
@@ -1,6 +1,14 @@
 var $productTabs = $('.js-product-tab');
+var $productShowcases = $('.js-product-showcase');
+
+//TODO (stew) look at query params to see which tab to activate
+$productShowcases.hide().filter(':first').show();
+$productTabs.first().addClass('product-tab--active');
 
 $productTabs.on('click', function() {
+  var $this = $(this);
   $productTabs.removeClass('product-tab--active');
-  $(this).addClass('product-tab--active');
+  $this.addClass('product-tab--active');
+  $productShowcases.hide();
+  $('#' + $this.attr('id') + '-showcase').show();
 });
