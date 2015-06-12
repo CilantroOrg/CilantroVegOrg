252e76f01fc75a770d9eee7ba1960367d4fc21f6
Stewart Smith <stewart@optimizely.com>
2015-06-11 18:27:59 -0700

    js logic for url hash and tab highlighting

--- website-guts/assets/js/pages/solutions.js
+++ website-guts/assets/js/pages/solutions.js
@@ -1,14 +1,27 @@
-var $productTabs = $('.js-product-tab');
-var $productShowcases = $('.js-product-showcase');
+var $productTabs = $('.js-product-tab'),
+    $productShowcases = $('.js-product-showcase'),
+    urlProduct = w.location.hash;
 
-//TODO (stew) look at query params to see which tab to activate
-$productShowcases.hide().filter(':first').show();
-$productTabs.first().addClass('product-tab--active');
+    // look at the hash to determine what tab to display
+if (!!urlProduct) {
+  var productTab = '#' + urlProduct.substring(1, urlProduct.length);
+  $(productTab + '-tab').addClass('product-tab--active');
+  $productShowcases.hide();
+  $(productTab + '-showcase').show();
+} else {
+  //otherwise open the first tab (testing)
+  $productShowcases.hide();
+  $('#testing-showcase').show();
+  $('#testing-tab').addClass('product-tab--active');
+}
 
 $productTabs.on('click', function() {
   var $this = $(this);
+  var tabTitle = $this.attr('id');
+
   $productTabs.removeClass('product-tab--active');
   $this.addClass('product-tab--active');
   $productShowcases.hide();
-  $('#' + $this.attr('id') + '-showcase').show();
+  tabTitle = tabTitle.substring(0, tabTitle.indexOf('-'));
+  $('#' +  tabTitle + '-showcase').show();
 });
