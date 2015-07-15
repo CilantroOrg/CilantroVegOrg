a2f946e80570e70699b7d4d6a01652def46d14a8
Bradley Heinz <bradley@optimizely.com>
2015-07-14 19:32:28 -0700

    Add Wistia godlen ticket - only enter email once

--- website-guts/assets/js/pages/opticon-sessions.js
+++ website-guts/assets/js/pages/opticon-sessions.js
@@ -1,1 +1,17 @@
-//this is our js file for the sesisons
+//Code below manages Wistia gating. Enter email address once, then rest are ungated
+wistiaEmbeds.onFind(function(video) {
+  var email = Wistia.localStorage("golden-ticket");
+  if (email) {
+    console.log('email being set');
+    video.setEmail(email);
+  }
+});
+wistiaEmbeds.bind("conversion", function(video, type, data) {
+  if (/^(pre|mid|post)-roll-email$/.test(type)) {
+    console.log('about to save on local storage');
+    Wistia.localStorage("golden-ticket", data);
+    for (var i = 0; i < wistiaEmbeds.length; i++) {
+      wistiaEmbeds[i].setEmail(data);
+    }
+  }
+});
