2c5af456dadf15dd5139ff76c3637574f6b31a6a
Stewart Smith <stewart@optimizely.com>
2015-07-27 11:16:40 -0700

    rename ops score to Most_Recent_Events_OPS__c and fill in form names

--- website-guts/assets/js/utils/oform_globals.js
+++ website-guts/assets/js/utils/oform_globals.js
@@ -253,8 +253,8 @@
     if(payload.company_type){
       reportingObject.Slns_Partner_Company_Type__c = payload.company_type;
     }
-    if(payload.event_ops){
-      reportingObject.event_ops = payload.event_ops;
+    if(payload.Most_Recent_Events_OPS__c){
+      reportingObject.Most_Recent_Events_OPS__c = payload.Most_Recent_Events_OPS__c;
     }
     if(payload.has_immediate_project__c){
       reportingObject.has_immediate_project__c = payload.has_immediate_project__c;
